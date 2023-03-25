7d10d66f47fb3da3dcb5fc327b90293762df06a5
r1vvy <s20seflemil@vea.lv>
2023-03-26 01:03:27 +0200

    Add: Map with fully functioning polygon drawing

--- src/components/map/MapComponent.js
+++ src/components/map/MapComponent.js
@@ -1,60 +1,30 @@
-import { MapContainer, TileLayer, FeatureGroup, Polygon, useMapEvents } from 'react-leaflet';
-import { EditControl } from 'react-leaflet-draw';
-import { useState } from 'react';
+import { MapContainer, TileLayer, FeatureGroup, Polygon } from "react-leaflet";
+import { EditControl } from "react-leaflet-draw";
+import { useState } from "react";
 
-import './MapComponent.css';
-import 'leaflet/dist/leaflet.css';
-import 'leaflet-draw/dist/leaflet.draw.css';
-import 'leaflet-draw/dist/leaflet.draw';
-
+import "./MapComponent.css";
+import "leaflet/dist/leaflet.css";
+import "leaflet-draw/dist/leaflet.draw.css";
+import "leaflet-draw/dist/leaflet.draw";
 
 function MapComponent() {
   const [position, setPosition] = useState([51.505, -0.09]);
   const [polygons, setPolygons] = useState([]);
 
-  function SearchBox() {
-    useMapEvents({
-      submit: (event) => {
-        event.preventDefault();
-        const form = event.target;
-        const searchText = form.search.value;
-        form.reset();
-        searchLocation(searchText);
-      }
-    });
-
-    async function searchLocation(searchText) {
-      const response = await fetch(`https://nominatim.openstreetmap.org/search?q=${searchText}&format=json`);
-      const data = await response.json();
-      if (data.length > 0) {
-        const { lat, lon } = data[0];
-        setPosition([lat, lon]);
-      }
-    }
-
-    return (
-      <div className="search-box">
-        <form>
-          <input type="text" name="search" placeholder="Search..." />
-          <button type="submit">Search</button>
-        </form>
-      </div>
-    );
+  function handleSetPolygons(e) {
+    setPolygons([...polygons, e.layer.getLatLngs()]);
   }
 
   return (
     <MapContainer center={position} zoom={13}>
       <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
-      <SearchBox />
       <FeatureGroup>
         {polygons.map((polygon, index) => (
           <Polygon key={index} positions={polygon} />
         ))}
         <EditControl
           position="topleft"
-          onCreated={(e) => {
-            setPolygons([...polygons, e.layer.getLatLngs()]);
-          }}
+          onCreated={handleSetPolygons}
           draw={{
             polyline: false,
             marker: false,
@@ -66,4 +36,5 @@ function MapComponent() {
       </FeatureGroup>
     </MapContainer>
   );
-} export default MapComponent;
+}
+export default MapComponent;
